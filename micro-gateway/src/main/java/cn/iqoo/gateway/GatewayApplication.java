4ae96b351da59ca8536c215db277f7bbf21e6035
汪剑 <wangjian@13322ty.com>
2018-05-21 15:00:18 +0800

    ~ gateway is ok.

--- micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
+++ micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
@@ -1,25 +1,28 @@
 package cn.iqoo.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
+@EnableDiscoveryClient
 public class GatewayApplication {
+
+    @Value("${ms.uri:http://httpbin.org:80}")
+    String uri;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r
-                        .path("/baidu")
-                        .filters(f -> f.addRequestHeader("xns", "baidu")
-                                .addResponseHeader("godada", "daddy"))
-                        .uri("http://baidu.com:80/")
+                        .path("/other/**")
+                        .filters(f -> f
+                                .addResponseHeader("daddy", "daddy"))
+                        .uri(uri)
                 )
                 .build();
     }
