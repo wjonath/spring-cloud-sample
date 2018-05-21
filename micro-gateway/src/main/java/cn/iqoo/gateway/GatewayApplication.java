1d3aeff5d71dcfafc0dd634cfd25f27fa1ba54ea
jonath <jonath@163.com>
2018-05-21 22:23:27 +0800

    ~ modify gateway version=M6

--- micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
+++ micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
@@ -3,13 +3,11 @@ package cn.iqoo.gateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class GatewayApplication {
 
     @Value("${test.uri:http://httpbin.org:80}")
