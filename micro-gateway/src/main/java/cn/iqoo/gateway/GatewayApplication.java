9e59a273134fec3b0640749850fa05062616a879
汪剑 <wangjian@13322ty.com>
2018-05-24 16:14:12 +0800

    ~ ready to RC1

--- micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
+++ micro-gateway/src/main/java/cn/iqoo/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class GatewayApplication {
--- micro-gateway/src/main/java/cn/iqoo/gateway/config/ResourceServerConfigurer.java
+++ micro-gateway/src/main/java/cn/iqoo/gateway/config/ResourceServerConfigurer.java
@@ -1,37 +1,37 @@
-/*
- * @project name: micro-build
- * @file name: WebSecurityConfigurer
- * @package Name: cn.iqoo.gateway.config
- * @date: 2018/5/23 15:32
- * @creator: wangjian-358
- * @line------------------------------
- * @modifier:
- * @date:
- * @content:
- */
-package cn.iqoo.gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-
-/**
- * ${TODO}
- *
- * @author wangjian-358
- * @date 2018/5/23 15:32
- * @see
- */
-@Configuration
-@EnableResourceServer
-public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/**")
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and().csrf().disable();
-    }
-}
+///*
+// * @project name: micro-build
+// * @file name: WebSecurityConfigurer
+// * @package Name: cn.iqoo.gateway.config
+// * @date: 2018/5/23 15:32
+// * @creator: wangjian-358
+// * @line------------------------------
+// * @modifier:
+// * @date:
+// * @content:
+// */
+//package cn.iqoo.gateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//
+///**
+// * ${TODO}
+// *
+// * @author wangjian-358
+// * @date 2018/5/23 15:32
+// * @see
+// */
+//@Configuration
+//@EnableResourceServer
+//public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .antMatcher("/**")
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and().csrf().disable();
+//    }
+//}
